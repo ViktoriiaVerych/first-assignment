fec5c3d2624228356512fe2eb58d36ce82607e2d
vs <santi.anjm@gmail.com>
2023-09-06 21:35:14 +0300

    1st milestone re

--- Untitled1.c
+++ Untitled1.c
@@ -1,16 +1,109 @@
-//int main() {
-//  printf("Hello World!");
-//  return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_TEXT_SIZE 1000
 
+typedef struct Line {
+    char* data;
+    struct Line* next;
+} Line;
+
+// creates a new line of text
+Line* createLine() {
+    Line* newLine = (Line*)malloc(sizeof(Line));
+    newLine->data = (char*)malloc(MAX_TEXT_SIZE);
+    newLine->data[0] = '\0';
+    newLine->next = NULL;
+    return newLine;
+}
+
+// appends text to the current line
+void appendText(Line* currentLine, const char* text) {
+    strcat(currentLine->data, text);
+}
+
+// starts a new linne
+Line* startNewLine(Line* currentLine) {
+    Line* newLine = createLine();
+    currentLine->next = newLine;
+    return newLine;
+}
+
+// prints the text
+void printText(Line* text) {
+    Line* current = text;
+    while (current != NULL) {
+        printf("%s\n", current->data);
+        current = current->next;
+    }
+}
 
 int main() {
-    char userInput;
-    printf("Choose the command: \n");
-    scanf("%d",&userInput);
-    printf("The command is not implemented");
+    Line* text = createLine();
+    Line* currentLine = text;
+    int command;
+    char input[MAX_TEXT_SIZE];
 
-    return 0;
+    while (1) {
+        printf("Choose the command:\n");
+        printf("1| Append text symbols to the end\n");
+        printf("2| Start a new line\n");
+        printf("3| Use files to saving the information\n");
+        printf("4| Use files to loading the information\n");
+        printf("5| Print the current text to console\n");
+        printf("6| Insert text by line and symbol index\n");
+        printf("7| Search for a substring\n");
+        printf("8| (Optional) Clear the console\n");
+        printf("9| Exit\n");
+
+        scanf("%d", &command);
+
+        switch (command) {
+            case 1:
+                printf("Enter text to append:\n");
+                scanf(" %[^\n]", input);
+                appendText(currentLine, input);
+                break;
+
+            case 2:
+                currentLine = startNewLine(currentLine);
+                printf("New line is started.\n");
+                break;
+
+            case 3:
+                printf("Current text:\n");
+                printText(text);
+                break;
+
+            case 4:
+                printf("");
+                break;
+
+            case 5:
+                printf("");
+                break;
+
+            case 6:
+                printf("");
+                break;
+
+            case 7:
+                printf("");
+                break;
+
+            case 8:
+                printf("Console has been cleared\n");
+                break;
+
+            case 9:
+                exit(0);
+
+
+            default:
+                printf("The command is not implemented.\n");
+                break;
+        }
+    }
 }
 
